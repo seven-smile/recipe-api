945b69f85a68df5990cadf5a95227df2f77b7a01
Miles <juniornyamile@gmail.com>
2024-08-31 14:46:50 +0000

    fd

--- app/core/migrations/0001_initial.py
+++ app/core/migrations/0001_initial.py
@@ -38,9 +38,10 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False)),
                 ('groups', models.ManyToManyField(
                     blank=True,
-                    help_text='The groups this user belongs to.'
-                              'A user will get all permissions '
-                               'granted to each of their groups.',
+                    help_text=(
+                        'The groups this user belongs to.'
+                        'A user will get all permissions '
+                        'granted to each of their groups.'),
                     related_name='user_set', related_query_name='user',
                     to='auth.Group',
                     verbose_name='groups')),
