294aed87cf0f654583bca50d48111729dbbca947
Miles <juniornyamile@gmail.com>
2024-09-01 20:16:26 +0000

    FEAT: Added test for normalizing email addresses.

--- app/core/migrations/0002_auto_20240901_1949.py
+++ app/core/migrations/0002_auto_20240901_1949.py
@@ -14,11 +14,24 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups'),
+            field=models.ManyToManyField(
+                blank=True,
+                help_text=(
+                    'The groups this user belongs to. A user will get'
+                    'all permissions granted to each of their groups.'),
+                related_name='user_set',
+                related_query_name='user',
+                to='auth.Group',
+                verbose_name='groups'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_superuser',
-            field=models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status'),
+            field=models.BooleanField(
+                default=False,
+                help_text=(
+                    'Designates that this user has all permissions without'
+                    'explicitly assigning them.'),
+                verbose_name='superuser status'),
         ),
     ]
