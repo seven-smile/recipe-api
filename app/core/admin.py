80f487a525cf6bc05b3d4e6d1978ad586b050b8c
Miles <juniornyamile@gmail.com>
2024-09-02 13:54:42 +0000

    added test for listing users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,16 @@
-from django.db import models  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
 
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
