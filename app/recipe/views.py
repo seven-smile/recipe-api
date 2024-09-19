aa3fd63a2dbb5e165ff6621344928857f0611291
Miles <juniornyamile@gmail.com>
2024-09-19 16:32:10 +0000

    FEAT:implementiing the recipe ilstig api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+"""
+Views for the recipe APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
