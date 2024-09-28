ae09db9b174e7c50bafa1814f8da20de00439623
Miles <juniornyamile@gmail.com>
2024-09-28 22:23:41 +0000

    resolving testing and linting issue

--- app/core/migrations/0006_auto_20240925_1215.py
+++ app/core/migrations/0006_auto_20240925_1215.py
@@ -21,7 +21,9 @@ class Migration(migrations.Migration):
                     serialize=False,
                     verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
