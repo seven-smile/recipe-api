066d50a3b5a0b4396a0c599c09f77c1a7ee71223
Miles <juniornyamile@gmail.com>
2024-10-03 13:20:09 +0000

    Feat: test for listing tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,80 +1,68 @@
 """
-Tests for models.
+Tests for the tags API.
 """
-from decimal import Decimal
-
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
 
-from core import models
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+
+TAGS_URL = reverse('recipe:tag-list')
 
 
 def create_user(email='user@example.com', password='testpass123'):
-    """Create a return a new user."""
-    return get_user_model().objects.create_user(email, password)
+    """Create and return a user."""
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
-class ModelTests(TestCase):
-    """Test models."""
+class PublicTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
 
-    def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successful."""
-        email = 'test@example.com'
-        password = 'testpass123'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password,
-        )
+    def setUp(self):
+        self.client = APIClient()
 
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
+    def test_auth_required(self):
+        """Test auth is required for retrieving tags."""
+        res = self.client.get(TAGS_URL)
 
-    def test_new_user_email_normalized(self):
-        """Test email is normalized for new users."""
-        sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@example.COM', 'test4@example.com'],
-        ]
-        for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
-            self.assertEqual(user.email, expected)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_new_user_without_email_raises_error(self):
-        """Test that creating a user without an email raises a ValueError."""
-        with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')
 
-    def test_create_superuser(self):
-        """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123',
-        )
+class PrivateTagsApiTests(TestCase):
+    """Test authenticated API requests."""
 
-        self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_staff)
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
 
-    def test_create_recipe(self):
-        """Test creating a recipe is successful."""
-        user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123',
-        )
-        recipe = models.Recipe.objects.create(
-            user=user,
-            title='Sample recipe name',
-            time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample receipe description.',
-        )
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
 
-        self.assertEqual(str(recipe), recipe.title)
+        res = self.client.get(TAGS_URL)
 
-    def test_create_tag(self):
-        """Test creating a tag is successful."""
-        user = create_user()
-        tag = models.Tag.objects.create(user=user, name='Tag1')
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-        self.assertEqual(str(tag), tag.name)
+    def test_tags_limited_to_user(self):
+        """Test list of tags is limited to authenticated user."""
+        user2 = create_user(email='user2@example.com')
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
