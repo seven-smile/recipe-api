8f9f8256b99557f297801ee4743890cfbd4b6fb2
Miles <juniornyamile@gmail.com>
2024-10-13 16:22:45 +0000

    media added

--- app/app/settings.py
+++ app/app/settings.py
@@ -135,7 +135,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -154,4 +153,8 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
 }
